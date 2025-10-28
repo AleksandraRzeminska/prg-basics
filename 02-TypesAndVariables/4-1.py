840cb61560268425a2cb91032b2a9500612c49ea
AleksandraRzeminska <aleksandrarzeminska@gmail.com>
2025-10-28 21:28:47 +0100

    dodaję kolejne zadania

---
+++ 02-TypesAndVariables/4-1.py
@@ -0,0 +1,8 @@
+###
+# Printing student's personal data
+#
+name = "Adam"
+age = "23"
+height = "182"
+age6 = (23 + 6)
+print(f"My name is {name}.I am {age} years old, and my height is {height} cm. In 6 years I will be {age6}.")
