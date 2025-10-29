15086fec1c9a6644d3d6e8da5e025190df89861c
AleksandraRzeminska <aleksandrarzeminska@gmail.com>
2025-10-29 19:37:47 +0100

    dodaję 3 pierwsze zadania z punktu 6

---
+++ 02-TypesAndVariables/6-1.py
@@ -0,0 +1,11 @@
+###
+# A program that calculates the number of characters
+# of your name, surname and full name
+#
+name = 'Aleksandra'   # replace Anna with your name
+surname = 'Rzemińska' # replace May with your surname
+characters_in_name = len(name)
+character_in_full_name = len(name + surname)
+print(f'Your name has {characters_in_name} characters')
+print(f'Your surname has {len(surname)} characters')
+print(f'Your full name has {character_in_full_name} characters') # with a space between name and surname
---
+++ 02-TypesAndVariables/6-2.py
@@ -0,0 +1,7 @@
+###
+# A program that prints your initials
+#
+name = 'Aleksandra'
+surname = 'Rzemińska'
+print(name[0])
+print(surname[0])
