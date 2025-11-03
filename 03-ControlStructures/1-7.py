338ccf226a8db1e8cd4e4fc4d03c0c55c45aeb6c
AleksandraRzeminska <aleksandrarzeminska@gmail.com>
2025-11-03 12:51:41 +0100

    dodaję zadania z lekcji

---
+++ 03-ControlStructures/1-7.py
@@ -0,0 +1,21 @@
+###
+# Program that calculates the employee's salary,
+# taking into account the possibility of receiving a bonus.
+#
+basic_salary = 5000
+total_salary = 0
+bonus = 0.15 # 15%
+is_bonus = input('Does the employee receive a bonus? (Y/N):') == 'Y'
+total_salary = basic_salary + bonus*basic_salary
+
+if is_bonus =='Y':
+    print('You receive a bonus')
+    print(f'Total salary: {basic_salary+bonus*basic_salary}')
+else:
+    print('You not receive a bonus')
+    print(f'Total salary: {basic_salary}')
+
+print(f'Basic salary: {basic_salary}')
+print(f'Bonus included? {is_bonus}')
+
+
