3bbb76c83202db5018ed702f3fc2877d29a099ea
Aleksandra Rzeminska <aleksandrarzeminska@gmail.com>
2025-11-24 12:55:58 +0100

    zadanka

---
+++ 07-Arrays/1-4.py
@@ -0,0 +1,19 @@
+###
+# Prints some array elements
+#
+arr = [2, 3, 7, 5, 4]
+
+#for i in (len(arr) <5):
+     #i = [0]
+     #i = i+1
+
+print(arr)
+print('Number of elements', len(arr))
+print('First value', arr[0])
+print('Second value', arr[1])
+print('Last value', arr[-1])
+print('Last but one value ', arr[3])
+print('Sum of the first and last value', arr[0]+ arr[-1])
+print('Middle value ', (len(arr)//2))
+#print('All array values separated by a single space', i )
+
